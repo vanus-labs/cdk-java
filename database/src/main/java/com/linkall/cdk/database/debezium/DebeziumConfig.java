bed9bf5d974e7fe78bc50397af7941e4ab1b1560
wenfeng <sxian.wang@gmail.com>
2022-12-22 02:24:08 +0800

    refactor: database package
    Signed-off-by: wenfeng <sxian.wang@gmail.com>

---
+++ database/src/main/java/com/linkall/cdk/database/debezium/DebeziumConfig.java
@@ -0,0 +1,56 @@
+package com.linkall.cdk.database.debezium;
+
+import java.util.Properties;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public abstract class DebeziumConfig extends com.linkall.cdk.config.SourceConfig {
+    protected abstract Properties getDebeziumProperties();
+
+    protected String tableFormat(String name, Stream<String> table) {
+        return table
+                .map(stream -> name + "." + stream)
+                .collect(Collectors.joining(","));
+    }
+
+    // common configuration
+    protected Properties getProperties() {
+        final Properties props = new Properties();
+//        props.setProperty("test.disable.global.locking","true");
+        // snapshot config
+        props.setProperty("snapshot.mode", "initial");
+        // DO NOT include schema change, e.g. DDL
+        props.setProperty("include.schema.changes", "false");
+        // disable tombstones
+        props.setProperty("tombstones.on.delete", "false");
+
+        props.setProperty("offset.storage", KvStoreOffsetBackingStore.class.getCanonicalName());
+//        if (getOffsetKey()!=null) {
+//            props.setProperty(
+//                    KvStoreOffsetBackingStore.OFFSET_STORAGE_KV_STORE_KEY_CONFIG, getOffsetKey());
+//        }
+//        Map<String, Object> configOffset = getConfigOffset();
+//        if (configOffset!=null && configOffset.size() > 0) {
+//            Converter valueConverter = new JsonConverter();
+//            Map<String, Object> valueConfigs = new HashMap<>();
+//            valueConfigs.put(JsonConverterConfig.SCHEMAS_ENABLE_CONFIG, false);
+//            valueConverter.configure(valueConfigs, false);
+//            byte[] offsetValue = valueConverter.fromConnectData(dbConfig.getDatabase(), null, configOffset);
+//            props.setProperty(
+//                    KvStoreOffsetBackingStore.OFFSET_CONFIG_VALUE,
+//                    new String(offsetValue, StandardCharsets.UTF_8));
+//        }
+
+        props.setProperty("offset.flush.interval.ms", "1000");
+
+        // https://debezium.io/documentation/reference/configuration/avro.html
+        props.setProperty("key.converter", "org.apache.kafka.connect.json.JsonConverter");
+        props.setProperty("key.converter.schemas.enable", "false");
+        props.setProperty("value.converter", "io.debezium.converters.CloudEventsConverter");
+        props.setProperty("value.converter.data.serializer.type", "json");
+        props.setProperty("value.converter.json.schemas.enable", "false");
+
+        props.putAll(getDebeziumProperties());
+        return props;
+    }
+}
