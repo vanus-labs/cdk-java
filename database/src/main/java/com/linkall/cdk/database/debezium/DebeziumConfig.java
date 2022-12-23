193bddc4ac5f4772d18359c4ab0cf8a2a280658d
delu <delu.xu@linkall.com>
2022-12-23 15:17:01 +0800

    chore: modify to dbz properties
    Signed-off-by: delu <delu.xu@linkall.com>

--- database/src/main/java/com/linkall/cdk/database/debezium/DebeziumConfig.java
+++ database/src/main/java/com/linkall/cdk/database/debezium/DebeziumConfig.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class DebeziumConfig extends SourceConfig {
-    @JsonProperty("custom_debezium")
+    @JsonProperty("dbz_properties")
     private Properties customDebezium;
 
     protected abstract Properties getDebeziumProperties();
