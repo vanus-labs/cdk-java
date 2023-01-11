551d3a2eb8c968bf9ccb8acb840d0d3c3cb06724
xdlbdy <xdlbdy@gmail.com>
2023-01-11 15:28:55 +0800

    feat: change debezium from cloudevent to sourcerecord
    Signed-off-by: xdlbdy <xdlbdy@gmail.com>

--- database/src/main/java/com/linkall/cdk/database/debezium/DebeziumConfig.java
+++ database/src/main/java/com/linkall/cdk/database/debezium/DebeziumConfig.java
@@ -1,8 +1,12 @@
 package com.linkall.cdk.database.debezium;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkall.cdk.config.SourceConfig;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,6 +16,8 @@ public abstract class DebeziumConfig extends SourceConfig {
     private Properties customDebezium;
 
     protected abstract Properties getDebeziumProperties();
+
+    protected abstract Object getOffset();
 
     protected String tableFormat(String name, Stream<String> table) {
         return table
@@ -31,33 +37,28 @@ public abstract class DebeziumConfig extends SourceConfig {
         props.setProperty("tombstones.on.delete", "false");
 
         props.setProperty("offset.storage", KvStoreOffsetBackingStore.class.getCanonicalName());
-//        if (getOffsetKey()!=null) {
-//            props.setProperty(
-//                    KvStoreOffsetBackingStore.OFFSET_STORAGE_KV_STORE_KEY_CONFIG, getOffsetKey());
-//        }
-//        Map<String, Object> configOffset = getConfigOffset();
-//        if (configOffset!=null && configOffset.size() > 0) {
-//            Converter valueConverter = new JsonConverter();
-//            Map<String, Object> valueConfigs = new HashMap<>();
-//            valueConfigs.put(JsonConverterConfig.SCHEMAS_ENABLE_CONFIG, false);
-//            valueConverter.configure(valueConfigs, false);
-//            byte[] offsetValue = valueConverter.fromConnectData(dbConfig.getDatabase(), null, configOffset);
-//            props.setProperty(
-//                    KvStoreOffsetBackingStore.OFFSET_CONFIG_VALUE,
-//                    new String(offsetValue, StandardCharsets.UTF_8));
-//        }
-
+        Object offset = getOffset();
+        if (offset!=null) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            byte[] offsetValue;
+            try {
+                offsetValue = objectMapper.writeValueAsBytes(offset);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("offset convert to json error", e);
+            }
+            props.setProperty(
+                    KvStoreOffsetBackingStore.OFFSET_CONFIG_VALUE,
+                    new String(offsetValue, StandardCharsets.UTF_8));
+        }
         props.setProperty("offset.flush.interval.ms", "1000");
 
-        // https://debezium.io/documentation/reference/configuration/avro.html
         props.setProperty("key.converter", "org.apache.kafka.connect.json.JsonConverter");
         props.setProperty("key.converter.schemas.enable", "false");
-        props.setProperty("value.converter", "io.debezium.converters.CloudEventsConverter");
-        props.setProperty("value.converter.data.serializer.type", "json");
-        props.setProperty("value.converter.json.schemas.enable", "false");
+        props.setProperty("value.converter.schemas.enable", "false");
 
         props.putAll(getDebeziumProperties());
-        if (customDebezium != null) {
+        if (customDebezium!=null) {
             props.putAll(customDebezium);
         }
         return props;
